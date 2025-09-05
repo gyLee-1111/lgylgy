a557e5e5b092b514902bfdeb19016d872143d51d
Lgy <lgylgy1120@naver.com>
2025-09-05 18:40:01 +0900

    배포수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5174, // 원하는 포트로 변경
-    proxy: {
-      '/api': {
-        target: 'http://lgy-spring-back-fxaqbsc0e8bdd3gd.koreacentral-01.azurewebsites.net',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    
     
   },
   resolve: {
