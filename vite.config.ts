5a8dfc3676350fd80e06c1970ec8f552d6e7baf1
Lgy <lgylgy1120@naver.com>
2025-09-05 18:31:38 +0900

    프론트 수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5174, // 원하는 포트로 변경
+    proxy: {
+      '/api': {
+        target: 'http://lgy-spring-back-fxaqbsc0e8bdd3gd.koreacentral-01.azurewebsites.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
     
   },
   resolve: {
